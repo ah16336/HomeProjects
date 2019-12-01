128d064f6dcf0e09b4e4ed73736726b720d22d7d
unknown <bryan@spartaglobal.com>
2019-12-01 14:04:50 +0000

    birthyear

--- SoccerAnalysis/SoccerAnalysis/SoccerAnalysis.py
+++ SoccerAnalysis/SoccerAnalysis/SoccerAnalysis.py
@@ -1,3 +1,13 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 
-print("Beth")
+year = pd.read_csv("birthyear.csv")
+
+x_years = list(range(1967, 2000))
+y_values = list(year.overall_score)
+
+plt.plot(x_years, y_values, color="GoldenRod")
+plt.title("Overall Score Vs Birth Year")
+plt.xlabel("Birth Year")
+plt.ylabel("Overall Score")
+plt.show()
