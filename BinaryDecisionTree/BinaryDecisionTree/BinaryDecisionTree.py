9e438eeea8880547b83190302eb0dfec83e89dec
unknown <bryan@spartaglobal.com>
2019-12-08 07:20:16 +0000

    tree building

--- BinaryDecisionTree/BinaryDecisionTree/BinaryDecisionTree.py
+++ BinaryDecisionTree/BinaryDecisionTree/BinaryDecisionTree.py
@@ -71,7 +71,7 @@ def get_split(dataset):
 				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
 	return {'index':b_index, 'value':b_value, 'groups':b_groups}
 
-## ALL TOGETHER ##
+## MID WAY TEST ##
 
 dataset = [[2.771244718,1.784783929,0],
 	[1.728571309,1.169761413,0],
@@ -83,8 +83,86 @@ dataset = [[2.771244718,1.784783929,0],
 	[7.444542326,0.476683375,1],
 	[10.12493903,3.234550982,1],
 	[6.642287351,3.319983761,1]]
-split = get_split(dataset)
-print('Split: [X%d < %.3f]' % ((split['index']+1), split['value'])) 
+#split = get_split(dataset)
+#print('Split: [X%d < %.3f]' % ((split['index']+1), split['value'])) 
 
 # Now we know where to divide a data on the x axis.
 
+### BUILDING A TREE ###
+
+## RECURSIVE SPLITING ##
+
+# Create a terminal node value
+def to_terminal(group):
+	outcomes = [row[-1] for row in group]
+	return max(set(outcomes), key=outcomes.count)
+
+# Deciding when to stop growing a tree, define two values:
+# Maximum Tree Depth: maximum number of levels in the tree; distance
+# from root node to terminal. 
+# Minimum Node Records: the minimum number of training patterns that 
+# a given node is responsible for. The number of records in its group to 
+# be split.
+
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, depth):
+	left, right = node['groups']
+	del(node['groups'])
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right) # groups what values we have left as terminal
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right) #both are terminal
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left) #makes left terminal
+	else:
+		node['left'] = get_split(left) # finds split value for left group
+		split(node['left'], max_depth, min_size, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right) #makes right terminal
+	else:
+		node['right'] = get_split(right) #finds split value for right group
+		split(node['right'], max_depth, min_size, depth+1)
+
+# Call upon the above function recursively to build the tree:
+# Build a decision tree
+def build_tree(train, max_depth, min_size):
+	root = get_split(train)
+	split(root, max_depth, min_size, 1)
+	return root
+
+# returning the tree:
+# Print a decision tree
+def print_tree(node, depth=0):
+	if isinstance(node, dict):
+		print('%s[X%d < %.3f]' % ((depth*' ', (node['index']+1), node['value'])))
+		print_tree(node['left'], depth+1)
+		print_tree(node['right'], depth+1)
+	else:
+		print('%s[%s]' % ((depth*' ', node)))
+ 
+## TREE TEST ##
+
+# using the same dataset above
+
+print("Depth = 1" + "\n")
+tree = build_tree(dataset, 1, 1)
+print_tree(tree)
+print("\n")
+
+print("Depth = 2" + "\n")
+tree = build_tree(dataset, 2, 1)
+print_tree(tree)
+print("\n")
+
+print("Depth = 3" + "\n")
+tree = build_tree(dataset, 3, 1)
+print_tree(tree)
+
+
+ 
